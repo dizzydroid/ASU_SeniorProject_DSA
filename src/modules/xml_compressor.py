c2b4647b69cd4dbcfe5dcda53694e431f0405ab5
Seif Elwarwary <seif.ahmed@3segypt.com>
2024-11-25 21:55:52 +0200

    edited compression and decompression for xml

--- src/modules/xml_compressor.py
+++ src/modules/xml_compressor.py
@@ -1,32 +1,18 @@
-import gzip
-import shutil
+import re
 class XMLCompressor:
     def __init__(self, input_path):
         self.input_path = input_path
 
     def compress(self, output_path):
-        with open(self.input_path, 'rb') as f_in:
-            with gzip.open(output_path, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
+        with open(self.input_path, 'r', encoding='utf-8') as file:
+            data = file.read()
 
+        compressed_data = re.sub(r'</[^>]+>', '</>', data)
 
+        with open(output_path, 'w', encoding='utf-8') as file:
+            file.write(compressed_data)
 
-input_xml = "/home/seifelwarwary/_colcode/ASU_SeniorProject_DSA/samples/large_sample.xml"
-output_gz = "/home/seifelwarwary/_colcode/ASU_SeniorProject_DSA/samples/output.gz"
-
-xml_data = """
-<users>
-    <user id="1">
-        <name>John Doe</name>
-        <posts>
-            <post>First post</post>
-            <post>Second post</post>
-        </posts>
-        <followers>
-            <follower id="2" />
-        </followers>
-    </user>
-</users>
-"""
-
+# Usage
+compressor = XMLCompressor('../../samples/large_sample.xml')
+compressor.compress('../../samples/output.compressed')
 
--- src/modules/xml_decompressor.py
+++ src/modules/xml_decompressor.py
@@ -1,12 +1,33 @@
-import gzip
-import shutil
-
-
+import re
 class XMLDecompressor:
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self, input_path):
+        self.input_path = input_path
 
     def decompress(self, output_path):
-        with gzip.open(self.file_path, 'rb') as f_in:
-            with open(output_path, 'wb') as f_out:
-                shutil.copyfileobj(f_in, f_out)
+        with open(self.input_path, 'r', encoding='utf-8') as file:
+            compressed_data = file.read()
+
+        open_tags = []
+
+        def record_open_tag(match):
+            tag_name = match.group(1)
+            open_tags.append(tag_name)
+            return match.group(0)
+
+        def replace_closing_tag(match):
+            if open_tags:
+                tag_name = open_tags.pop()
+                return f'</{tag_name}>'
+            return match.group(0)
+
+        # Record all open tags
+        compressed_data = re.sub(r'<([^/!\s>]+)[^>]*>', record_open_tag, compressed_data)
+        # Replace </> with the correct closing tags
+        decompressed_data = re.sub(r'</>', replace_closing_tag, compressed_data)
+
+        with open(output_path, 'w', encoding='utf-8') as file:
+            file.write(decompressed_data)
+
+# Usage
+decompressor = XMLDecompressor('../../samples/output.compressed')
+decompressor.decompress('../../samples/decompressed_output.xml')
