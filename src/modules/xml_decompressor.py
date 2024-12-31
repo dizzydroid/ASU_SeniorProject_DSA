1e7747acfc178b7951ad534894a2ab503afe04b9
Abdelrahman Hany <boody753107@gmail.com>
2024-12-31 04:47:16 +0200

    Fixed XMLDecompress

--- src/modules/xml_decompressor.py
+++ src/modules/xml_decompressor.py
@@ -1,33 +1,50 @@
-import re
 class XMLDecompressor:
     def __init__(self, input_path):
         self.input_path = input_path
 
+    def extract_tag(self, line, start):
+        # Extract tag from line
+        tag = "" 
+        for char in line[start:]:
+            if char == ' ' or char == '>':
+                break
+            elif char == '<':
+                continue
+            elif char == '?' or char == '!':
+                return -1
+            tag += char
+        return tag
+
     def decompress(self, output_path):
         with open(self.input_path, 'r', encoding='utf-8') as file:
-            compressed_data = file.read()
+            lines = file.readlines()
+            open_tags = []
 
-        open_tags = []
-
-        def record_open_tag(match):
-            tag_name = match.group(1)
-            open_tags.append(tag_name)
-            return match.group(0)
-
-        def replace_closing_tag(match):
-            if open_tags:
-                tag_name = open_tags.pop()
-                return f'</{tag_name}>'
-            return match.group(0)
-
-        # Record all open tags
-        compressed_data = re.sub(r'<([^/!\s>]+)[^>]*>', record_open_tag, compressed_data)
-        # Replace </> with the correct closing tags
-        decompressed_data = re.sub(r'</>', replace_closing_tag, compressed_data)
-
+            for i, line in enumerate(lines):
+                for j, char in enumerate(line):
+                    if char == '<':
+                        tag = self.extract_tag(line, j)
+                        
+                        if tag == -1:
+                            continue
+                        # Add opening tag to stack
+                        if tag[0] != '/':
+                            open_tags.append(tag)
+                        else:
+                            # If closing tag with no opening tag
+                            if not open_tags:
+                                raise Exception(f"Missing opening tag for {tag[1:]}")
+                            else:
+                                # Replace closing tag with correct closing tag
+                                tag_name = open_tags.pop()
+                                lines[i] = lines[i][:j] + f'</{tag_name}>' + lines[i][j + len(tag) + 2:]
+                                
+        # Write decompressed data to output file
         with open(output_path, 'w', encoding='utf-8') as file:
-            file.write(decompressed_data)
+            file.writelines(lines)
+
+
 
 # Usage
-#decompressor = XMLDecompressor('../../samples/output.compressed')
-#decompressor.decompress('../../samples/decompressed_output.xml')
+decompressor = XMLDecompressor(r'../samples/compressed.xml')
+decompressor.decompress(r'../samples/decompressed.xml')
